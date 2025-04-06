8838b62f603e822669769c4b611adec3df209fa7
XoDeR <xoder.vs@gmail.com>
2025-04-06 18:37:14 +0200

    corrected layout

--- client02/src/common/layout.tsx
+++ client02/src/common/layout.tsx
@@ -8,12 +8,6 @@ interface LayoutProps {
 export const Layout: React.FC<LayoutProps> = ({ children }: LayoutProps) => {
   return (
     <div className="flex flex-col min-h-screen bg-gray-50">
-      <nav className="flex-1 ml-4 text-lg">
-        <Routes>
-          <Route path="/timespans/list" element={<span>Timesheet</span>} />
-          <Route path="/user/tags" element={<span>User</span>} />
-        </Routes>
-      </nav>
       <aside className="fixed top-0 left-0 h-full bg-gray-800 text-white shadow-lg transform">
         <nav className="flex flex-col p-4 space-y-4">
           <div>
@@ -32,7 +26,9 @@ export const Layout: React.FC<LayoutProps> = ({ children }: LayoutProps) => {
           </div>
         </nav>
       </aside>
-      {children}
+      <main className="flex-1 mt-16 md:ml-64">
+        <div className="p-4">{children}</div>
+      </main>
     </div>
   )
 }
